f753a99c5aaf625dd26b0dd4c5ea1faa45bd65f6
Umut Topalak <100470201+umuttopalak@users.noreply.github.com>
2024-09-03 15:22:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+from datetime import datetime
+
+from flask import Flask
+
+app = Flask(__name__)
+
+last_request_date = None
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/health-check')
+def health_check():
+    global last_request_date
+    return {'status': 'OK', 'last_request_date': last_request_date}
+
+
+@app.route('/electric-check')
+def electric_check():
+    global last_request_date
+    last_request_date = datetime.now()
+    return {'status': 'OK', 'message': 'last request date updated', 'last_request_date': last_request_date}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
